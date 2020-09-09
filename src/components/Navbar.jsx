0c5dd42c00a2dcc3596a2885d204152026233f05
Alfredo Martinez <psialfredomtz@gmail.com>
2020-09-09 01:00:49 -0500

    Flexbox in Typography

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {AppBar, Toolbar, Typography, makeStyles, IconButton} from '@material-ui/core'
+import {AppBar, Toolbar, Typography, makeStyles, IconButton, Button} from '@material-ui/core'
 import MenuIcon from '@material-ui/icons/Menu';
 
 
@@ -7,6 +7,9 @@ const useStyles = makeStyles(theme => ({
   offset: theme.mixins.toolbar,
   menuButton: {
     marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1
   },
 }))
 
@@ -19,9 +22,12 @@ export default function Navbar() {
           <IconButton className={classes.menuButton} color="inherit" aria-label="menu">
             <MenuIcon/>
           </IconButton>
-          <Typography variant="h6">
+          <Typography variant="h6" className={classes.title}>
             Alfredo
           </Typography>
+          <Button variant="text" color="inherit">
+            LOGIN
+          </Button>
         </Toolbar>
       </AppBar>
       <div className={classes.offset}></div>
