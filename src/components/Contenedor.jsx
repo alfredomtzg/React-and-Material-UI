05a067c23ddd89f8dcb069dbd29037daf07d89b1
Alfredo Martinez <psialfredomtz@gmail.com>
2020-09-09 13:32:33 -0500

    agree Navbar

--- src/components/Contenedor.jsx
+++ src/components/Contenedor.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import Navbar from './Navbar'
+import {
+  makeStyles
+} from '@material-ui/core'
+
+const estilos = makeStyles(theme => ({
+  root: {
+    display: 'flex'
+  }
+}))
 
 const Contenedor = () => {
-    return(
-        <div>Contenido</div>
-    )
+  const classes = estilos()
+
+  return(
+    <div className={classes.root}>
+      <Navbar/>
+      Contenido
+    </div>
+  )
 }
 
 export default Contenedor;
