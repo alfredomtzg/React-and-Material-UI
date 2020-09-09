ce207b18a003a509d5df748e07741ee471a00fc0
Alfredo Martinez <psialfredomtz@gmail.com>
2020-09-09 02:35:01 -0500

    Agree divider and new button share

--- src/components/Listas.jsx
+++ src/components/Listas.jsx
@@ -8,19 +8,40 @@ import {
 } from '@material-ui/core'
 import SportsEsportsIcon from '@material-ui/icons/SportsEsports';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import ShareIcon from '@material-ui/icons/Share';
 
 
 const Listas = () => {
   return(
     <div>
       <List component='nav'>
+
         <ListItem button>
           <ListItemIcon>
             <SportsEsportsIcon/>
           </ListItemIcon>
-          <ListItemText primary='Mi primer elemento'/>
+          <ListItemText primary='Juegos online'/>
         </ListItem>
+
+        <ListItem button>
+          <ListItemIcon>
+            <ShoppingCartIcon/>
+          </ListItemIcon>
+          <ListItemText primary='Juegos disponibles'/>
+        </ListItem>
+
+        <Divider/>
+
+        <ListItem button>
+          <ListItemIcon>
+            <ShareIcon/>
+          </ListItemIcon>
+          <ListItemText primary='Comparte con tus amigos'/>
+        </ListItem>
+
       </List>
+
+      
     </div>
   )
 }
