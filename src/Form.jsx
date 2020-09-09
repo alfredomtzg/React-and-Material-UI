75035cc604093df5524ac1bc96300d920e01d9c3
Alfredo Martinez <psialfredomtz@gmail.com>
2020-09-08 21:22:44 -0500

    Top NavBar

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,12 +1,12 @@
 // import react
 import React from 'react'
 // import FormControl
-import { FormControlControl, FormControl } from '@material-ui/core';
 
 function Login() {
     return (
-        <h1>Aquí va un formulario</h1>
+            <p>hola</p>
     )
 }
 
 export default Login;
+
--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Login from './Form';
 import App from './App';
+import AppBar from './AppBar'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Login />
+    <AppBar />
   </React.StrictMode>,
   document.getElementById('root')
 );
