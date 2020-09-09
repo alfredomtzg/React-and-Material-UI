b7888389808d79fddace21a49f54394b7f7b5cf2
Alfredo Martinez <psialfredomtz@gmail.com>
2020-09-09 12:59:42 -0500

    contenedor

--- src/TemaGlobal.jsx
+++ src/TemaGlobal.jsx
@@ -1,22 +1,15 @@
 import React from 'react'
-import Button from '@material-ui/core/Button'
-import { MuiThemeProvider } from '@material-ui/core/styles'
+import { ThemeProvider } from '@material-ui/core/styles'
 import theme from './temaConfig'
-import Navbar from './components/Navbar'
-import Listas from './components/Listas'
-import Oculto from './components/Oculto'
+import Contenedor from './components/Contenedor'
+
 
 function TemaGlobal() {
-    return(
-        <MuiThemeProvider theme={theme}>
-            <Navbar/>
-            <Button variant="contained" color="primary">
-                contained
-            </Button>
-            <Listas/>
-            <Oculto/>
-        </MuiThemeProvider>
-    )
+  return(
+    <ThemeProvider theme={theme}>
+      <Contenedor/>
+    </ThemeProvider>
+  )
 }
 
 export default TemaGlobal;
---
+++ src/components/Contenedor.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Contenedor = () => {
+    return(
+        <div>Contenido</div>
+    )
+}
+
+export default Contenedor;
