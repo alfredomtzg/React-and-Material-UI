88940daf4abac0944d7c7f1c549750b05d5216fb
Alfredo Martinez <psialfredomtz@gmail.com>
2020-09-09 12:49:28 -0500

    Hidden

--- src/components/Oculto.jsx
+++ src/components/Oculto.jsx
@@ -1,11 +1,39 @@
 import React from 'react'
+import {withWidth, Typography, Hidden, Button} from '@material-ui/core'
 
-const Oculto = () => {
-    return(
-        <>
-        <h1>hola</h1>
-        </>
-    )
+const Oculto = (props) => {
+	return(
+		<>
+			<Typography variant="h6" color="initial">
+				Ancho:{ props.width}
+			</Typography>
+
+      <Hidden xsDown >
+        <Button variant="contained" color="primary">
+          xs
+        </Button>
+      </Hidden>
+
+      <Hidden smDown >
+        <Button variant="contained" color="primary">
+          sm
+        </Button>
+      </Hidden>
+
+      <Hidden mdUp >
+        <Button variant="contained" color="secondary">
+          md
+        </Button>
+      </Hidden>
+
+      <Hidden only={['lg','xs']} >
+        <Button variant="contained" color="secondary">
+          only
+        </Button>
+      </Hidden>
+
+		</>
+	)
 }
 
-export default Oculto;
+export default withWidth()(Oculto)
