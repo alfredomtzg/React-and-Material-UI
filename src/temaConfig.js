884c1d70e256bf0f1fff928ec5edf1cf2438feaf
Alfredo Martinez <psialfredomtz@gmail.com>
2020-09-08 23:39:41 -0500

    Change palette color

--- src/temaConfig.js
+++ src/temaConfig.js
@@ -1,7 +1,12 @@
 import {createMuiTheme} from '@material-ui/core/styles'
+import purple from '@material-ui/core/colors/purple';
 
 const theme = createMuiTheme({
-
+    palette: {
+        primary:{
+            main: purple[600],
+        }
+    }
 })
 
 export default theme;
