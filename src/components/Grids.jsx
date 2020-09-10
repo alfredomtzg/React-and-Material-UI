2569cf5796cf67cc13cf34df8ea6d53e114228c6
Alfredo Martinez <psialfredomtz@gmail.com>
2020-09-10 01:14:24 -0500

    Grid xs

--- src/components/Grids.jsx
+++ src/components/Grids.jsx
@@ -6,11 +6,31 @@ const Grids = () => {
     <>
       <Grid container >
         <Grid item xs={12}>
-          <Box color="primary.contrastText" bgcolor="primary.main">
-            hola
+          <Box color="primary.main"   border={1}>
+            xs 12
           </Box>
         </Grid>
-        
+
+        <Grid item xs={8}>
+          <Box color="primary.main" mt={1} border={1}>
+            xs 8
+          </Box>
+        </Grid>
+        <Grid item xs={8}>
+          <Box color="primary.main" mt={1} border={1}>
+            xs 8
+          </Box>
+        </Grid>
+        <Grid item xs={4}>
+          <Box color="primary.main" ml={1} mt={1} border={1}>
+            xs 4
+          </Box>
+        </Grid>
+        <Grid item xs={1}>
+          <Box color="primary.main" mt={1} border={1}>
+            xs 1
+          </Box>
+        </Grid>
       </Grid>
     </>
   )
