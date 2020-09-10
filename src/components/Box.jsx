538ac3e6b26d3b1d9f9f52c5d52e34770be9e3f5
Alfredo Martinez <psialfredomtz@gmail.com>
2020-09-10 00:31:21 -0500

    Boxing

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -4,8 +4,20 @@ import { Box } from '@material-ui/core'
 const Boxin = () => {
   return (
     <>
-      <Box>
-        
+      <Box color="secondary.main">
+        Caja con color secundary
+      </Box>
+      <Box color="primary.contrastText" bgcolor="primary.main">
+        Caja con color contrastText y fondo primary
+      </Box>
+      <Box m={2} color="primary.main">
+        caja con margin2
+      </Box>
+      <Box ml={3} color="primary.main">
+        caja con marginleft 1
+      </Box>
+      <Box p={4} color="primary.main">
+        caja con padding 4
       </Box>
     </>
   )
